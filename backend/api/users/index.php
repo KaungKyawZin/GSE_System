cb3045d0715abed6a73e297e01e58a6b3062c142
Kaung Kyaw Zin <kaungkyawzin@fdbbank.com>
2026-07-19 12:30:43 +0630

    show role and create user file add

--- backend/api/users/index.php
+++ backend/api/users/index.php
@@ -4,15 +4,12 @@ header("Content-Type: application/json");
 require_once __DIR__ . "/../../config/database.php";
 $user_id = $_GET["user_id"] ?? null;
 
-if ($user_id != 2) {
-
-    http_response_code(400);
-
+if($user_id != 2){
+    http_response_code(403);
     echo json_encode([
         "success" => false,
-        "message" => "user_id is incorrect"
+        "message" => "Access denied"
     ]);
-
     exit;
 }
 try {
