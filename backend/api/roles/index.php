41d551b30a0b5fcad4ea117534ff69ef6392e0d7
Kaung Kyaw Zin <kaungkyawzin@fdbbank.com>
2026-07-20 21:15:48 +0630

    update backend

--- backend/api/roles/index.php
+++ backend/api/roles/index.php
@@ -3,14 +3,7 @@ header("Content-Type: application/json");
 
 require_once __DIR__ . "/../../config/database.php";
 $user_id = $_GET["user_id"] ?? null;
-if($user_id != 2){
-    http_response_code(403);
-    echo json_encode([
-        "success" => false,
-        "message" => "Access denied"
-    ]);
-    exit;
-}
+
 try {
 $sql = "select * from roles ORDER BY role_id DESC";
 $stmt = $pdo->prepare($sql);
--- backend/api/users/index.php
+++ backend/api/users/index.php
@@ -4,14 +4,6 @@ header("Content-Type: application/json");
 require_once __DIR__ . "/../../config/database.php";
 $user_id = $_GET["user_id"] ?? null;
 
-if($user_id != 2){
-    http_response_code(403);
-    echo json_encode([
-        "success" => false,
-        "message" => "Access denied"
-    ]);
-    exit;
-}
 try {
     $sql = "SELECT * FROM users ORDER BY user_id DESC";
     $stmt = $pdo->prepare($sql);
