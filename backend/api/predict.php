80d32d28f34abf511ab02b1f957e038a9088b2ac
Kaung Kyaw Zin <kaungkyawzin@fdbbank.com>
2026-08-06 13:19:47 +0630

    update predict.php

--- backend/api/predict.php
+++ backend/api/predict.php
@@ -1,7 +1,7 @@
 <?php
 
 header("Content-Type: application/json");
-
+require_once __DIR__ . "/../config/database.php";
 
 $data=json_decode(
     file_get_contents("php://input"),
@@ -15,7 +15,7 @@ $vehicle_id=$data["vehicle_id"];
 $python = "C:\\Users\\kaungkyawzin\\AppData\\Local\\Python\\pythoncore-3.14-64\\python.exe";
 
 
-$script = "C:\\Users\\kaungkyawzin\\Downloads\\GSE_System\\ai-service\\predict_api.py";
+$script = "C:\\Users\\kaungkyawzin\\GSE_System\\ai-service\\predict_api.py";
 
 
 $command = "\"$python\" \"$script\" $vehicle_id 2>&1";
